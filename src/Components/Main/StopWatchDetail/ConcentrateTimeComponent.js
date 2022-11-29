db3caf4b8d2163ba8220ccf2fd2727419bfb66b4
kknyapple <kknyapple@gmail.com>
2022-11-29 15:23:02 +0900

    feat: 집중 시간 측정 기능
    일시정시 버튼을 누르면 시간이 멈췄다가, 이어하기 버튼을 누르면 다시 시간 측정
    정지 버튼 누르면 집중 시간 초기화

--- src/Components/Main/StopWatchDetail/ConcentrateTimeComponent.js
+++ src/Components/Main/StopWatchDetail/ConcentrateTimeComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useRecoilState } from "recoil";
+
+import { pauseClicked } from "../../../recoil/concentrate";
 
 const ConcentrateTime = styled.div`
   display: flex;
@@ -9,14 +13,38 @@ const ConcentrateTime = styled.div`
   padding-right: 40px;
 `;
 
-const Name = styled.p``;
-const Number = styled.p``;
+const Title = styled.p``;
+const Time = styled.p``;
 
 const ConcentrateTimeComponent = () => {
+  const [pause, setPause] = useRecoilState(pauseClicked);
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    if (pause === false) setTimeout(() => setSecond(second + 1), 1000);
+
+    if (second > 59) {
+      setSecond(0);
+      setMinute(minute + 1);
+    }
+    if (minute > 59) {
+      setMinute(0);
+      setHour(hour + 1);
+    }
+  }, [pause, second]);
+
   return (
     <ConcentrateTime>
-      <Name>집중시간</Name>
-      <Number>00:00:00</Number>
+      <Title>집중시간</Title>
+      <Time>
+        {" "}
+        {`${String(hour).padStart(2, "0")}:${String(minute).padStart(
+          2,
+          "0"
+        )}:${String(second).padStart(2, "0")}`}
+      </Time>
     </ConcentrateTime>
   );
 };
