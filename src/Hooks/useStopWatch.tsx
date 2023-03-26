24ff6784efbdef3f7938356c051f0742b63e720b
kknyapple <kknyapple@gmail.com>
2023-03-26 22:55:53 +0900

    refactor: 상태 변수 및 함수 이름 변경
    hour, minute, second를 time으로 변경
    startTime 함수를 updateTime으로 변경

--- src/Hooks/useStopWatch.tsx
+++ src/Hooks/useStopWatch.tsx
@@ -1,31 +1,26 @@
 import { useEffect, useState } from "react";
 
-const useStopWatch = (
-  condition: boolean,
-  currentStartTime: number
-): [number, number, number] => {
-  const [hour, setHour] = useState(0);
-  const [minute, setMinute] = useState(0);
-  const [second, setSecond] = useState(0);
+const useStopWatch = (condition: boolean, currentStartTime: number) => {
+  const [time, setTime] = useState(new Date(0));
 
-  const startTime = () => {
-    const now = new Date(Date.now() - currentStartTime);
-
-    setSecond(now.getUTCSeconds());
-    setMinute(now.getUTCMinutes());
-    setHour(now.getUTCHours());
+  const updateTime = () => {
+    setTime(new Date(Date.now() - currentStartTime));
   };
 
   useEffect(() => {
-    if (condition === true) {
-      startTime();
+    if (condition) {
+      updateTime();
       let timerId = setInterval(() => {
-        startTime();
+        updateTime();
       }, 1000);
 
       return () => clearInterval(timerId);
     }
   }, [condition]);
+
+  const hour = time.getUTCHours();
+  const minute = time.getUTCMinutes();
+  const second = time.getUTCSeconds();
 
   return [hour, minute, second];
 };
