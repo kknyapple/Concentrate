eee91b2f9a2090fa816b9079c67785ac8e931f6a
kknyapple <kknyapple@gmail.com>
2023-03-30 16:49:00 +0900

    design: 안내 문구 css 수정

--- src/Components/Main/Record/MessageButtonComponent.tsx
+++ src/Components/Main/Record/MessageButtonComponent.tsx
@@ -6,6 +6,8 @@ const ButtonWrapper = styled.div`
   display: inline-block;
   margin-left: 5px;
   margin-right: 5px;
+  width: 14px;
+  height: 25px;
 `;
 
 const HoverMessage = styled.span`
@@ -17,12 +19,19 @@ const HoverMessage = styled.span`
   border-radius: 6px;
   position: absolute;
   z-index: 1;
-  margin-left: 135px;
+  margin-left: 152px;
   transform: translateX(-50%);
-  margin-top: 25px;
   font-size: 10px;
   width: 300px;
 `;
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 25px;
+`;
+
 const MessageButtonComponent = (props) => {
   const [isHovering, setIsHovering] = useState(false);
 
@@ -31,18 +40,21 @@ const MessageButtonComponent = (props) => {
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <svg
-        width="14"
-        height="14"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M11 18H13V16H11V18ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM12 6C9.79 6 8 7.79 8 10H10C10 8.9 10.9 8 12 8C13.1 8 14 8.9 14 10C14 12 11 11.75 11 15H13C13 12.75 16 12.5 16 10C16 7.79 14.21 6 12 6Z"
-          fill="#F5F5F5"
-        />
-      </svg>
+      <Div>
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11 18H13V16H11V18ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM12 6C9.79 6 8 7.79 8 10H10C10 8.9 10.9 8 12 8C13.1 8 14 8.9 14 10C14 12 11 11.75 11 15H13C13 12.75 16 12.5 16 10C16 7.79 14.21 6 12 6Z"
+            fill="#F5F5F5"
+          />
+        </svg>
+      </Div>
+
       <HoverMessage visible={isHovering}>{props.text}</HoverMessage>
     </ButtonWrapper>
   );
